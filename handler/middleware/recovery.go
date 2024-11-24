92913b34ad92bab2be58f79448dfcf403e9b920d
katsuya yamaguchi <y.katsuya1116@gmail.com>
2024-11-24 09:56:32 +0900

    refact: recoveryログ出力追加

--- handler/middleware/recovery.go
+++ handler/middleware/recovery.go
@@ -1,12 +1,16 @@
 package middleware
 
-import "net/http"
+import (
+	"log"
+	"net/http"
+)
 
 func Recovery(h http.Handler) http.Handler {
 	fn := func(w http.ResponseWriter, r *http.Request) {
 		// TODO: ここに実装をする
 		defer func() {
 			if err := recover(); err != nil {
+				log.Println(err)
 				http.Error(w, "internal server error", http.StatusInternalServerError)
 			}
 		}()
