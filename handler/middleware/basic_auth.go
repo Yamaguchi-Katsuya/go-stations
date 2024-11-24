3547d14f6cdb57911ce148dbef92b0c7612ea9fb
katsuya yamaguchi <y.katsuya1116@gmail.com>
2024-11-24 09:55:53 +0900

    refact: BASIC認証改修

--- handler/middleware/basic_auth.go
+++ handler/middleware/basic_auth.go
@@ -1,26 +1,34 @@
 package middleware
 
 import (
+	"log"
 	"net/http"
 	"os"
 )
 
+var (
+	basicAuthUserID   string
+	basicAuthPassword string
+)
+
+func init() {
+	basicAuthUserID = os.Getenv("BASIC_AUTH_USER_ID")
+	basicAuthPassword = os.Getenv("BASIC_AUTH_PASSWORD")
+
+	if basicAuthUserID == "" || basicAuthPassword == "" {
+		log.Fatal("BASIC_AUTH_USER_ID or BASIC_AUTH_PASSWORD is not set")
+	}
+}
+
 func BasicAuth(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		userID := os.Getenv("BASIC_AUTH_USER_ID")
-		password := os.Getenv("BASIC_AUTH_PASSWORD")
-		if userID == "" || password == "" {
-			http.Error(w, "basic auth is required", http.StatusUnauthorized)
-			return
-		}
-
 		user, pass, ok := r.BasicAuth()
 		if !ok {
 			w.Header().Set("WWW-Authenticate", `Basic realm="restricted"`)
 			http.Error(w, "basic auth is required", http.StatusUnauthorized)
 			return
 		}
-		if user != userID || pass != password {
+		if user != basicAuthUserID || pass != basicAuthPassword {
 			http.Error(w, "user or password is incorrect", http.StatusUnauthorized)
 			return
 		}
